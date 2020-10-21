31aa04558a647cf5a42547493ff351b981fc1b04
bkd1516 <bkd1516@naver.com>
2020-10-22 00:53:57 +0900

    Test2

--- TEST_PROJECT/TEST_PROJECT/Form1.cs
+++ TEST_PROJECT/TEST_PROJECT/Form1.cs
@@ -20,6 +20,7 @@ namespace TEST_PROJECT
         private void Form1_Load(object sender, EventArgs e)
         {
             Console.WriteLine("TEST");
+            Console.WriteLine("TEST2");
         }
     }
 }
